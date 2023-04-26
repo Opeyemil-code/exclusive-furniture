01403772f4d440dfe9f0e5bb1b1a13b6e6188e70
opeyemi-code <opeyemitoheeb31@gmail.com>
2023-04-26 23:31:20 +0100

    fixed the header

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function Header(props) {
     return (
-        <header className="flex flex-row bg-white lg:w-2/3 mx-auto lg:p-6 space-x-12 text-center shadow-xl w-11/12 rounded-lg">
-          <h1 className="text-2xl font-bold font-Rochester lg:mt-0 lg:ml-0 mt-4 ml-6">Exclusive</h1>
+        <header className="flex flex-row bg-white lg:w-2/3 mx-auto lg:p-6 space-x-12 text-center shadow-xl w-11/12 rounded-lg p-3 mt-4">
+          <h1 className="text-3xl font-bold font-Rochester lg:mt-0 lg:ml-0 mt-4 ml-2">Exclusive</h1>
 
           <div className="hidden lg:block">
            <ul className="flex flex-row space-x-8 ml-52 text-lg font-Roboto text-darkblue ">
